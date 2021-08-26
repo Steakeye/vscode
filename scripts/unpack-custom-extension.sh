c8d67c4bdc4b312870df4da35e5b5ee3be1b9eda
Andrew Keats <andrew.keats@git.live>
2021-08-26 13:28:25 +0100

    wip: working on custom script to unpack vsix for use in vscode web

---
+++ scripts/unpack-custom-extension.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Unpack custom extension..."
+
+if [ $# -eq 0 ]; then
+	echo "Pass in a version like ./scripts/unpack-custom-extension.sh <path to VSIX>"
+	echo "Failed to run script"
+	exit 1
+fi
+
+source_path=$1
+target_folder=$2
+
+function abortScript {
+echo "Aborting script"
+	exit 1
+}
+
+echo VSIX package: $source_path
+
+if [ -f "$source_path" ]; then
+	if [ "${source_path##*.}" != "vsix" ]; then
+		echo "File $source_path exists bu is not a valid VSIX file"
+		abortScript
+	fi
+
+if [ -z "$target_folder" ]; then
+target_folder=$(basename "$source_path" ".vsix")
+echo "Target folder not specified, setting to $target_folder"
+	fi
+else
+echo "$source_path does not exist..."
+abortScript
+fi
+
+echo "Source VSIX: $source_path"
+echo "Target folder: $target_folder"
+
+target_path="custom-extensions/$target_folder"
+
+echo target path: $target_path
+
+if [ -d "$target_path" ]; then
+	echo "Folder '$target_path' already exists"
+	echo "Deleting existing destination folder..."
+	rm -rf "$target_path"
+fi
+
+exit $?
